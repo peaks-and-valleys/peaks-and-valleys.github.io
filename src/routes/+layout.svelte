7f571c158a0a246264b5161ff2a595ccdc2b681a
peaksvndvalleys <39687819+peaksvndvalleys@users.noreply.github.com>
2024-07-28 18:56:12 +0900

    :add: CSS, layout file determined which typeface will be used

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import type { LayoutData } from './$types';
+    import '../styles/styles.css';
+    
+</script>
+
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+<p>あのイーハトーヴォのすきとおった風、夏でも底に冷たさをもつ青いそら、うつくしい森で飾られたモリーオ市、郊外のぎらぎらひかる草の波。</p>
