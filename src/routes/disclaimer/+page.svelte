bea27a796058b09887da7c9b427ffecab6a1347d
peaksvndvalleys <39687819+peaksvndvalleys@users.noreply.github.com>
2024-09-21 22:23:27 +0900

    :add: #9 disclaimer page
    Claude を使ったら本文が一瞬で完成した

---
+++ src/routes/disclaimer/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<svelte:head>
+	<title>Peaks and Valleys - disclaimer</title>
+    <meta name="description" content="disclaimer">
+</svelte:head>
+
+<div class="main">
+    <h1>disclaimer</h1>
+    <div class="content-box">
+		{@html data.content}
+	</div>
+</div>
