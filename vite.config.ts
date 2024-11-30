4824a8b259aec4b2c650fcfdb4ab3ac763961aa3
peaksvndvalleys <39687819+peaksvndvalleys@users.noreply.github.com>
2024-11-30 19:39:52 +0900

    feat(vite.config.ts): add watch options for polling to improve file change detection in certain environments

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,17 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	server: {
-		host: true
+		host: true,
+		watch: {
+			usePolling: true,
+			interval: 1000,
+			binaryInterval: 1500
+		}
 	},
 	plugins: [sveltekit()],
 	build: {
 		rollupOptions: {
-		    external: ['fs']
+			external: ['fs']
 		}
 	}
 });
