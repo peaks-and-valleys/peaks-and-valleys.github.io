3d0b67ae29ca1933343bc0b4b136cc7e2cd73a86
peaks-and-valleys <39687819+peaks-and-valleys@users.noreply.github.com>
2025-04-08 17:29:28 +0900

    style(page.svelte): refactor cover image class names for better semantics and maintainability style(app.scss): update SCSS imports to include new index styles for cover component feat(index.scss): create new SCSS file for cover component styles to improve organization and modularity

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,13 +13,13 @@
 	<meta name="description" content="底が見えるほど深く、手が届くほど広い" />
 </svelte:head>
 
-<div class="cover-image">
-	<video class="cover-component" loop autoplay muted playsinline>
+<div class="p-cover">
+	<video class="p-cover__image" loop autoplay muted playsinline>
 		<source src="images/index/background.mp4" type="video/mp4" />
 		<track kind="captions" />
 	</video>
 	<img
-		class="cover-component"
+		class="p-cover__image"
 		src="images/index/peaksandvalleys.svg"
 		alt="Peaks and Valleys と手書き風文字で書かれたロゴです"
 	/>
@@ -33,30 +33,3 @@
 		</p>
 	</article>
 </article>
-
-<style>
-	.cover-image {
-		display: grid;
-		grid-template-columns: 1fr;
-		grid-template-rows: 1fr;
-		width: 100%;
-	}
-	.cover-component {
-		grid-column: 1;
-		grid-row: 1;
-		display: flex;
-		justify-content: center;
-		width: 100%;
-		flex-wrap: wrap;
-	}
-	.cover-component:nth-child(1) {
-		z-index: 1;
-		width: 100%;
-	}
-	.cover-component:nth-child(2) {
-		z-index: 2;
-		mix-blend-mode: difference;
-		width: 100%;
-		margin: auto;
-	}
-</style>
