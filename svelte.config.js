491a2b5890207d54a446febbcc9bdfa27ffe1b2e
peaks-and-valleys <39687819+peaks-and-valleys@users.noreply.github.com>
2025-04-24 13:40:20 +0900

    feat(package.json): add rehype-sanitize dependency for improved HTML sanitization feat(svelte.config.js): integrate rehype-sanitize plugin into mdsvex options for enhanced security

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,13 @@
 import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import rehypeSanitize from 'rehype-sanitize';
 
 /** @type {import('mdsvex').MdsvexOptions} */
 const mdsvexOptions = {
 	extensions: ['.svx'],
-}
+	rehypePlugins: [rehypeSanitize]
+};
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
