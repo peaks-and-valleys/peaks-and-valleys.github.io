8208b97086027f0871c966eda991d552c8061023
Tohga Shimoji <39687819+peaks-and-valleys@users.noreply.github.com>
2025-04-24 13:59:39 +0900

    Feature/replace markdown parser (#50)
    * feat(package.json): add mdsvex dependency to support MDX-like syntax in Svelte components
    feat(svelte.config.js): integrate mdsvex for preprocessing .svx files alongside existing preprocessors
    
    * feat(blog): implement blog post management with caching and metadata handling
    
    - Add functions to retrieve all posts, published posts, and posts by slug.
    - Introduce caching mechanism for performance optimization.
    - Define Post and PostMetadata types for better type safety.
    - Refactor blog page server load to utilize new post retrieval functions.
    - Update blog page to display posts using the new data structure.
    
    * refactor(blog): remove old +page.server.ts and replace with +page.ts for improved data loading and structure
    feat(blog): update +page.svelte to utilize new data structure and dynamic content loading for better performance and user experience
    
    * chore(blog): Replace all blog posts
    
    * chore(package.json): remove unused dependencies to clean up package.json
    chore(vite.config.ts): remove 'fs' from external rollup options as it is no longer needed
    
    * feat(package.json): add rehype-sanitize dependency for improved HTML sanitization
    feat(svelte.config.js): integrate rehype-sanitize plugin into mdsvex options for enhanced security
    
    * fix(blog): update date formatting in blog post previews to use Japanese locale for consistency and clarity

--- src/routes/blog/post/[slug]/+page.svelte
+++ src/routes/blog/post/[slug]/+page.svelte
@@ -1,24 +1,42 @@
 <script lang="ts">
 	import type { PageData } from './$types';
+	import type { Component } from 'svelte';
+	import { onMount } from 'svelte';
 
 	interface Props {
 		data: PageData;
 	}
 
 	let { data }: Props = $props();
+
+	const { meta, isDraft } = data;
+
+	let Content: Component;
+	let contentLoaded = $state(false);
+
+	onMount(() => {
+		Content = data.content;
+		contentLoaded = true;
+	});
 </script>
 
 <svelte:head>
-	<title>{data.items?.title} - Peaks and Valleys</title>
-	<meta name="description" content={data.items?.description} />
+	<title>{meta.title} - Peaks and Valleys</title>
+	<meta name="description" content={meta.description} />
 </svelte:head>
 
 <article class="p-articles">
 	<hgroup>
-		<h1 class="p-articles__heading u-rounded-border">{data.items?.title}</h1>
-		<p style="text-align: center;"><time>{data.items?.date}</time> / {data.items?.description}</p>
+		<h1 class="p-articles__heading u-rounded-border">{meta.title}</h1>
+		<p style="text-align: center;">
+			<time datetime={meta.date}>{new Date(meta.date).toLocaleDateString('ja')}</time> - {meta.description}
+		</p>
 	</hgroup>
 	<article class="p-article u-rounded-border">
-		{@html data.items?.content}
+		{#if contentLoaded}
+			<Content />
+		{:else}
+			<p>記事を読み込み中...</p>
+		{/if}
 	</article>
 </article>
