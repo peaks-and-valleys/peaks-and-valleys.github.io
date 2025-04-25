39f646f40ad090f27d346106cd053c8c3c3df71b
Tohga Shimoji <39687819+peaks-and-valleys@users.noreply.github.com>
2025-04-25 11:52:22 +0900

    Feature/ogp properties (#57)
    * feat(head-properties): add head-properties component for dynamic page metadata
    feat(ogp.jpg): add Open Graph image for better social media sharing experience
    
    * feat(routes): replace inline head elements with HeadProperties component for better code organization and reusability
    fix(routes): update header link text from "index" to "home" for improved clarity
    style(routes/blog): change blog heading text to Japanese for localization consistency

---
+++ src/lib/components/head-properties.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import { page } from '$app/state';
+
+	const baseTitle: string = 'Peaks and Valleys';
+	export let pageTitle: string = "";
+	let wholeTitle = baseTitle;
+
+	if (pageTitle != "") {
+		wholeTitle = pageTitle + " - " + baseTitle;
+	}
+
+	export let description: string = '底が見えるほど深く、手が届くほど広い';
+
+	const baseUrl: string = "https://peaks-and-valleys.net";
+	let wholeUrl: string = baseUrl + page.url.pathname;
+	// ToDo: remove grouping () path
+
+	export let pageType: string = "website";
+</script>
+
+<svelte:head>
+	<title>{wholeTitle}</title>
+	<meta name="description" content={description} />
+	<meta property="og:description" content={description} />
+	<meta property="og:title" content={wholeTitle} />
+	<meta property="og:url" content={wholeUrl} />
+	<meta property="og:image" content="{baseUrl}/images/ogp.jpg" />
+	<meta property="og:site_name" content={baseTitle} />
+	<meta property="og:locale" content="ja" />
+	<meta property="og:type" content={pageType} />
+</svelte:head>
