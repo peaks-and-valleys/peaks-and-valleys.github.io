a70e1d8859b1685760f186eace0eebc769223b9f
peaks-and-valleys <39687819+peaks-and-valleys@users.noreply.github.com>
2025-05-15 18:47:48 +0900

    #83: feat(package.json): add shiki package for enhanced syntax highlighting support feat(svelte.config.js): integrate shiki for syntax highlighting in mdsvex options to improve code presentation in markdown files

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,25 @@
-import { mdsvex } from 'mdsvex';
+import { mdsvex, escapeSvelte } from 'mdsvex';
 import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import rehypeExternalLinks from 'rehype-external-links';
+import { createHighlighter } from 'shiki';
+
+/** Syntax highlighting options */
+const theme = 'poimandres';
+const highlighter = await createHighlighter({
+	themes: [theme],
+	langs: ['css', 'javascript', 'sass', 'typescript', 'yaml']
+});
 
 /** @type {import('mdsvex').MdsvexOptions} */
 const mdsvexOptions = {
 	extensions: ['.svx'],
+	highlight: {
+		highlighter: async (code, lang) => {
+			const html = escapeSvelte(highlighter.codeToHtml(code, { lang, theme }));
+			return `{@html \`${html}\` }`;
+		}
+	},
 	rehypePlugins: [[rehypeExternalLinks, { rel: 'external noopener noreferrer' }]]
 };
 
