f207d7886c5b1f1b2c8ae82ca9df6916ca7947be
ぴ <39687819+peaks-and-valleys@users.noreply.github.com>
2025-07-08 15:28:52 +0900

    Fix: update contents (#97)
    * docs(about/+page.svelte): improve about page content with list format and links
    
    * style(+page.svelte): adjust margin on image to use spacing variable
    
    * docs(content.svx): add documentation for prefers-reduced-motion media query

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -10,37 +10,26 @@
 	<h1>このサイトについて</h1>
 	<p>いわゆる個人サイトです。</p>
 	<h2>やっていること</h2>
-	<h3>IT エンジニアリング</h3>
-	<p>
-		<a href="https://github.com/peaks-and-valleys" rel="me external noopener noreferrer"
-			>GitHub プロフィール</a
-		>
-	</p>
-	<p>
-		必要になればだいたいなんでもやります（が、低レイヤには苦手意識があります…）。最近はフロントエンドやデザイン、UI/UX
-		のことに興味があります。
-	</p>
-	<h3>グラフィックデザイン</h3>
-	<p>
-		<a
-			href="https://www.behance.net/peaks-and-valleys"
-			rel="me external noopener noreferrer"
-			translate="no">Behance プロフィール</a
-		>
-	</p>
-	<p>
-		いろいろ手を出していますが、主にアブストラクトな表現を好みます。タイポグラフィーにも強い関心があって、よく無目的にフォントを漁って時間を溶かしたりしています。稀に動画も作ります。
-	</p>
-	<h3>文章</h3>
-	<p>
-		上記に関連したことや、それ以外の私的なことなどをブログに書いています。自身の言語化の練習のために書いているもので、読みづらいかもしれません。文体もいきなり常体になりますが、気にしないでください。
-	</p>
-	<p>
-		また、文章を書き散らすための補助ツールとして <a
-			href="https://scrapbox.io/peaks-and-valleys/"
-			rel="me external noopener noreferrer">Cosense (Scrapbox)</a
-		> を使っています。
-	</p>
+	<ul>
+		<li>
+			IT エンジニアリング - <a
+				href="https://github.com/peaks-and-valleys"
+				rel="me external noopener noreferrer">GitHub</a
+			>
+		</li>
+		<li>
+			グラフィックデザイン - <a
+				href="https://www.behance.net/peaks-and-valleys"
+				rel="me external noopener noreferrer"
+				translate="no">Behance</a
+			>
+		</li>
+		<li>
+			文章 - <a href="https://scrapbox.io/peaks-and-valleys/" rel="me external noopener noreferrer"
+				>Cosense (Scrapbox)</a
+			>
+		</li>
+	</ul>
 	<h2>連絡先</h2>
 	<p></p>
 	<address>
