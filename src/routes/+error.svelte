2832d4cc646f676a560f6b36c7c2f96a60d0f28f
peaks-and-valleys <39687819+peaks-and-valleys@users.noreply.github.com>
2025-07-16 17:44:00 +0900

    feat(+error.svelte): add error page with heading and back button #37

---
+++ src/routes/+error.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { page } from '$app/state';
+	import HeadProperties from '$lib/components/HeadProperties.svelte';
+</script>
+
+<HeadProperties pageTitle={String(page.status)} />
+
+<article class="p-articleContainer" role="status">
+	<h1>{page.status}</h1>
+	<p>{page.error?.message}</p>
+	<button on:click={() => history.back()}>前のページに戻る</button>
+</article>
+
+<style lang="scss">
+	button {
+		margin-block-start: var(--spacing-s);
+		font-size: 1rem;
+		color: var(--bg-primary);
+		background-color: var(--c-secondary);
+		padding: var(--spacing-xxs);
+		border: none;
+
+		@media (any-hover) {
+			&:hover {
+				background-color: var(--c-accent-blue);
+				@media (prefers-reduced-motion: no-preference) {
+					transition: 200ms;
+				}
+			}
+		}
+	}
+</style>
