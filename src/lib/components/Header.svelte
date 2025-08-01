8fe04d9e2c6c4c426bb852be9b8557355c44e0fb
peaks-and-valleys <39687819+peaks-and-valleys@users.noreply.github.com>
2025-08-01 11:57:42 +0900

    feat(components): add Header and Footer components to improve site navigation and branding
    refactor(routes/+layout.svelte): update layout to use new Header and Footer components, remove redundant navigation links

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+	import { page } from '$app/state';
+</script>
+
+<header>
+	<h1>
+		<a href="/" aria-current={page.url.pathname === '/' ? 'page' : undefined}>Peaks and Valleys</a>
+	</h1>
+	<nav aria-label="メインナビゲーション">
+		<ul>
+			<li>
+				<a href="/blog" aria-current={page.url.pathname.startsWith('/blog') ? 'true' : undefined}
+					>blog</a
+				>
+			</li>
+		</ul>
+	</nav>
+</header>
+
+<style lang="scss">
+	header {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: var(--spacing-xxs);
+		view-transition-name: header;
+
+		h1 {
+			margin-block-start: 0;
+		}
+
+		a {
+			color: var(--c-primary);
+			text-decoration: none;
+		}
+
+		nav ul li {
+			list-style: none;
+			margin: 0;
+			font-size: 0.875rem;
+
+			a {
+				@media (any-hover) {
+					&:hover {
+						text-decoration: underline 2px currentColor;
+					}
+				}
+
+				&[aria-current='true'] {
+					text-decoration: underline 2px currentColor;
+				}
+			}
+		}
+	}
+</style>
